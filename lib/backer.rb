ed6c28df8496015026262b6ac3100030775190fe
Philippe Timothe <philippetimothe@gmail.com>
2020-05-15 13:24:45 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,10 @@
 class Backer
-  attr_accessor 
+  attr_accessor :backed_projects
   attr_reader :name
   
-  def initialize(name) 
+  def initialize(name, backed_projects = []) 
     @name = name
-    @backed_porjects = []
+    
   end
   
   
