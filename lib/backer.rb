988b409694f7a0be42623b167193f5fa6d7914ef
Philippe Timothe <philippetimothe@gmail.com>
2020-05-15 14:12:39 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class Backer
+  attr_accessor 
+  attr_reader :name, :backed_projects
+  
+  def initialize(name, backed_projects = []) 
+    @name = name
+    @backed_projects = backed_projects
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+  
+  
+  
+  
+  
+end 
